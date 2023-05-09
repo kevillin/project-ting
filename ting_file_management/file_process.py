17dbeaa09d977e058740d881c4c786f6e9f0eeb1
Kevillin Santos <kevillin_santos@hotmail.com>
2023-05-09 18:43:08 -0300

    feat: Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    output = {"nome_do_arquivo": path_file,
+              "qtd_linhas": len(txt_importer(path_file)),
+              "linhas_do_arquivo": txt_importer(path_file)}
+    instance.enqueue(output)
+
+    return sys.stdout.write(str(output))
 
 
 def remove(instance):
