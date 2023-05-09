fa850e485effdc4bef6732208f391d27c036494a
Kevillin Santos <kevillin_santos@hotmail.com>
2023-05-09 19:17:59 -0300

    feat: requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -16,7 +16,13 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance or len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    path_file = instance.dequeue()['nome_do_arquivo']
+
+    return sys.stdout.write(
+        f"Arquivo {path_file} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
