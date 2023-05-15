509c374c762e140f3e210ca12335f73ac53a2be2
Kevillin Santos <kevillin_santos@hotmail.com>
2023-05-15 17:20:33 -0300

    feat: Requisito 7 com ajuda do Mario Monteiro

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        linhas_arquivo = instance.search(i)["linhas_do_arquivo"]
+        nome_arquivo = instance.search(i)["nome_do_arquivo"]
+        ocorrencias = [
+            {"linha": j + 1}
+            for j, linha in enumerate(linhas_arquivo)
+            if word.lower() in linha.lower()
+        ]
+
+        if ocorrencias:
+            if any(o["linha"] for o in ocorrencias):
+                result.append(
+                    {
+                        "palavra": word,
+                        "arquivo": nome_arquivo,
+                        "ocorrencias": ocorrencias,
+                    }
+                )
+    return result
 
 
 def search_by_word(word, instance):
